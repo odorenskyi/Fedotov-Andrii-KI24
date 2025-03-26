6acddf7c1181e856c28583624d4f0b1dcffcf623
Andrey <andrejfedotov012@gmail.com>
2025-03-26 14:37:13 +0200

    Створено бібліотеку LibOutputName. Реалізовано функцію виводу імені розробника.

---
+++ lab08/prj/LibOutputName/LibOutputName/OutputName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void Information() {
+    SetConsoleOutputCP(1251);
+    cout << "\n ----------------------------------------\n"
+
+        << "|   Andrey Fedotov, CUNTU, Kropyvnickyi   |\n"
+
+        << "|   Андрій Федотов, ЦНТУ, Кропивницький   |\n"
+
+        << "\n -------- (c) All Rights Reserved --------\n\n";
+}
+
