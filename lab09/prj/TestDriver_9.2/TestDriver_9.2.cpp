a60a658e98f79b94fe7165f15a67a2d1931d1576
Andrey <andrejfedotov012@gmail.com>
2025-04-05 12:55:28 +0300

    Test: Тести для функцій 9.1-9.3 (модульні) TestDriver /main.cpp з контрольними прикладами .Exe файли TestDriver 9.1 - 9.3 додано до папки Software

---
+++ lab09/prj/TestDriver_9.2/TestDriver_9.2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "ModulesFedotov.h"
+using namespace std;
+
+
+void test_headCircumference() {
+    int testValues[9] = { 53, 55, 57, 59, 61, 48, 50, 52, 63 };
+    string expectedResults[9] = {
+        "Категорія: Дорослі\nРозмір шолома: XS\n",
+        "Категорія: Дорослі\nРозмір шолома: S\n",
+        "Категорія: Дорослі\nРозмір шолома: M\n",
+        "Категорія: Дорослі\nРозмір шолома: L\n",
+        "Категорія: Дорослі\nРозмір шолома: XL\n",
+        "Категорія: Діти\nРозмір шолома: S\n",
+        "Категорія: Діти\nРозмір шолома: M\n",
+        "Категорія: Діти\nРозмір шолома: L\n"
+    };
+
+    for (int i = 0; i < 9; i++) {
+        string result = headCircumference(testValues[i]);
+
+
+        if (result == expectedResults[i]) {
+            cout << "Тест кейс #" << (i + 1) << " | Окружність голови: " << testValues[i] << " см\n";
+            cout << "Очікуваний результат:\n" << expectedResults[i];
+            cout << "Фактичний результат:\n" << result;
+            cout << "Результат: PASSED\n";
+        }
+        else {
+            cout << "Тест кейс #" << (i + 1) << endl;
+            cout << "Розмір не знайдений!" << endl;
+            cout << "Фактичний Результат: FAILED\n";
+        }
+        cout << "--------------------------------------\n";
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    test_headCircumference();
+    system("pause");
+    return 0;
+}
---
+++ lab09/prj/TestDriver_9.3/TestDriver_9.3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "ModulesFedotov.h"
+using namespace std;
+
+void test_countBits() {
+    unsigned int testInputs[] = { 1, 3, 255, 1024, 2047, 1023, 512, 10000, 500, 8999 };
+    unsigned int expectedOutputs[] = { 31,  30, 24, 31, 11, 10, 1, 0, 26,  7 };
+
+    for (int i = 0; i < 10; i++) {
+        unsigned int result = countBits(testInputs[i]);
+        cout << "Тест кейс " << (i + 1) << " | Вхід: " << testInputs[i] << "\n";
+        cout << "Очікуваний результат: " << expectedOutputs[i] << "\n";
+        cout << "Фактичний результат: " << result << "\n";
+
+        if (result == expectedOutputs[i]) {
+            cout << "Результат: PASSED\n";
+        }
+        else {
+            cout << "Результат: FAILED\n";
+        }
+        cout << "--------------------------------------\n";
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    test_countBits();
+    system("pause");
+    return 0;
+}
