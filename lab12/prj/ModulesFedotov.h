a8604ecc94c38ebf738e059cc5cd52469884cb02
Andrey <andrejfedotov012@gmail.com>
2025-06-05 00:07:16 +0300

    Task 12.1. Проаналізовано вимоги до задачі. Та реалізован клас у заголовковому фаайлі ModulesFedotov.h

---
+++ lab12/prj/ModulesFedotov.h
@@ -0,0 +1,50 @@
+#ifndef MODULESFEDOTOV_H_INCLUDED
+#define MODULESFEDOTOV_H_INCLUDED
+
+class ClassLab12_Fedotov {
+public:
+    ClassLab12_Fedotov();
+    ClassLab12_Fedotov(float a, float b);
+    void setA(float a);
+    void setB(float b);
+    float getA();
+    float getB();
+    float getArea();
+    float getPerimeter();
+private:
+    float a = 0.0;
+    float b = 0.0;
+};
+
+ClassLab12_Fedotov::ClassLab12_Fedotov() {}
+
+ClassLab12_Fedotov::ClassLab12_Fedotov(float a, float b) {
+    this->a = a;
+    this->b = b;
+}
+
+void ClassLab12_Fedotov::setA(float a) {
+    this->a = a;
+}
+
+void ClassLab12_Fedotov::setB(float b) {
+    this->b = b;
+}
+
+float ClassLab12_Fedotov::getA() {
+    return a;
+}
+
+float ClassLab12_Fedotov::getB() {
+    return b;
+}
+
+float ClassLab12_Fedotov::getArea() {
+    return a * b;
+}
+
+float ClassLab12_Fedotov::getPerimeter() {
+    return 2 * (a + b);
+}
+
+#endif 
